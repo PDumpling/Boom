a9b10ce8fa6afbedacbf52dd7083e73716a00ddc
PDumpling <77103653+PDumpling@users.noreply.github.com>
2021-01-24 13:24:28 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    x = 0
+    y = -60
+})
 controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
     controller.moveSprite(man, 100, 100)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    let mySprite: Sprite = null
     projectile = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -20,9 +23,27 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        `, mySprite, 0, 60)
+        `, man, x, y)
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    x = -60
+    y = 0
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    x = 60
+    y = 0
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    x = 0
+    y = 60
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.trail, 500)
+})
+let beest: Sprite = null
 let projectile: Sprite = null
+let y = 0
+let x = 0
 let man: Sprite = null
 tiles.setTilemap(tilemap`level1`)
 man = sprites.create(img`
@@ -46,5 +67,24 @@ man = sprites.create(img`
 man.setStayInScreen(true)
 scene.cameraFollowSprite(man)
 game.onUpdateInterval(5000, function () {
-	
+    beest = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . e e e e . . . . . . . 
+        . . . . . e e f e . . . . . . . 
+        . . . . . e e e e e f . . . . . 
+        . . e e e e e e e e f . . . . . 
+        . . e e e e e e . . . . . . . . 
+        . . e e e e e e . . . . . . . . 
+        . . e . e . . e . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    beest.follow(man, 40)
+    beest.setPosition(randint(0, 10), randint(0, 10))
 })
